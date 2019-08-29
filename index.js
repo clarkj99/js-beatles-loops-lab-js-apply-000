eca458a9de456381b8033c472da72799afe14d33
considerate-glossary-7180 <clarkandkathy@gmail.com>
2019-08-29 00:38:10 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,10 +2,11 @@
 function theBeatlesPlay(musicians, instruments) {
   var description = []; 
   
-  for (i = 0; i < musicians.length; i++) {
+  for (var i = 0; i < musicians.length; i++) {
     var string = musicians[i] + " plays " + instruments[i];
     description.push(string);
   }
+  
   return description;
 }
 
